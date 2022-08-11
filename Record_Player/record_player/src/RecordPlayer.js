05634ec066e542b1d8e2fbca32ef35d584dc27eb
CarolineMadison <brownleecaroline@gmail.com>
2022-08-10 19:41:09 -0500

    convert to react

---
+++ Record_Player/record_player/src/RecordPlayer.js
@@ -0,0 +1,42 @@
+import "./styles.css"
+
+
+function RecordPlayer() {
+  return (
+    
+
+
+
+
+<div className="turntable">
+
+	<span className="speedStrobe"></span>
+	<span className="redLight"></span>
+	<span className="whiteLight"></span>
+
+	<div className="container">
+	  <span className="circle small"></span>
+	  <span className="circle big"></span>
+	  <span className="circle dot"></span>
+
+		<div className="elektra-logo">
+		<span className="shapes rect"></span>
+		<span className="shapes triangle-down"></span>
+		<span className="shapes triangle-up"></span>
+		</div>
+
+
+	  <span className="titles"><h3>THE DOORS</h3><h4>Strange Days</h4></span>
+	</div>
+	<span className="arm-container">
+		<span className="arm"></span>
+		<span className="thorn"></span>
+	</span>
+
+</div>
+
+
+  );
+}
+
+export default RecordPlayer;
